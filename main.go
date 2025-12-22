cf96a6eb460821cfb2454fe9e27e1d8619051588
Sebastian <sebascova18@gmail.com>
2025-12-21 19:23:18 -0500

    agrega services y repo al flujo del api

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"example/repository"
+	"example/services"
 )
 
 
@@ -17,12 +19,21 @@ func main() {
      err:= godotenv.Load(); if err != nil{
 	 log.Fatalf("Error loading .env file: %v", err) 
 	 }
+
 	 db.Conectar()
 	 defer db.DB.Close()
 
+    repo:= repository.NewJugadorRepo(db.DB)
+	service:= services.NewJugadorService(*repo)
+
 	r:= gin.Default()
 	r.Use(gin.Logger())
-	handlers.MethodAsignment(r)
+	handlers.MethodAsignment(r,service)
+ 
+    
+    log.Println("Servidor iniciado en el puerto 8080")
 	log.Fatal(r.Run(":8080"))
-	log.Println("Servidor iniciado en el puerto 8080")
+
+
+
 }
